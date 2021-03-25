99677c5f829e96c83cfa3e0745cf2ca931c7fb2f
osamabutt5212 <osamabutt5212@gmail.com>
2021-03-25 07:34:43 -0700

    Linear Layout practice

---
+++ LinearLayout/app/src/main/java/com/usamaaziz01/a05_linearlayouts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.usamaaziz01.a05_linearlayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
