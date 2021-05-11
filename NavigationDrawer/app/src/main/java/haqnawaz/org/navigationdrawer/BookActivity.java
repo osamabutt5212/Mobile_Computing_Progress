fdc32c92af0c923f2784ba7eb8d33effc24cff69
osamabutt5212 <osamabutt5212@gmail.com>
2021-05-11 14:10:12 -0700

    Navigation Drawer practice

---
+++ NavigationDrawer/app/src/main/java/haqnawaz/org/navigationdrawer/BookActivity.java
@@ -0,0 +1,17 @@
+package haqnawaz.org.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+        Toast.makeText(getApplicationContext(),"Retur is Clicked",Toast.LENGTH_LONG).show();
+
+    }
+}
